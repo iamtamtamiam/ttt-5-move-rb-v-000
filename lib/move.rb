e0c96ddf22d835f9c4ea196845a5fea381d10389
Tam Vu <vutam_a@hotmail.com>
2019-08-11 23:10:21 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,12 @@ def input_to_index(user_input)
 user_input.to_i - 1
 end
 
+<<<<<<< HEAD
 def move(board, position, character = 'X')
   board[position] = character
+=======
+def move (board, position, character = 'X')
+  board = 
+  
+>>>>>>> f1a856a99f44ad28000bbc311a7fb1c3024d7e4b
 end
